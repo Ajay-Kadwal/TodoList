6b64c088f5c441ce0211b1d87be8a34865ece3a6
Ajay Kadwal <ajaykadwal.work@gmail.com>
2024-11-18 15:28:52 +0530

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by AJAY KADWAL on 18/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
